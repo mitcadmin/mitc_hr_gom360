7e261c806d5565ee1519233fba2747dde89a8400
Chaupi Ghambi <cghambi@mitc.mw>
2026-08-18 21:24:35 +0000

    Release v0.0.1

---
+++ mitc_hr_gom360/mitc_hr_gom360/doctype/obdx_payment_batch/obdx_payment_batch.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, MITC and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("OBDX Payment Batch", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ mitc_hr_gom360/mitc_hr_gom360/doctype/rbm_account/test_rbm_account.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, MITC and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRBMAccount(FrappeTestCase):
+	pass
